ba37264c8e03e7d6c67507e3d98fdaaa2f3ae67e
GrantLicata <grant.licata@gmail.com>
2023-02-05 14:06:25 -0800

    styling add note component

--- client/src/components/Add.js
+++ client/src/components/Add.js
@@ -41,26 +41,25 @@ function Add() {
 
 return ( 
   <>
-    <form onSubmit={addNote}>
-      <h3> Add New Note </h3>
+    <form className="m-6" onSubmit={addNote}>
+      <h3 className="mb-4 text-xl"> Add New Note </h3>
       <div>
         <div>
-          <label>Title</label>
-          <input type="text" name="title" value={note.title} onChange={onChangeTitle} placeholder="Title" required />
+          <label className="font-bold">Title: </label>
+          <input className="bg-slate-100 rounded-md pl-2" type="text" name="title" value={note.title} onChange={onChangeTitle} placeholder="Enter title" required />
         </div>
-        <div>
-          <label> Note <span> * </span> </label>
-        <EditorToolbar toolbarId={'t1'}/>
-        <ReactQuill
-          theme="snow"
-          value={note.body}
-          onChange={onChangeBody}
-          placeholder={"Write something awesome..."}
-          modules={modules('t1')}
-          formats={formats}
-        />
+        <div className="mt-3">
+          <EditorToolbar toolbarId={'t1'}/>
+          <ReactQuill
+            theme="snow"
+            value={note.body}
+            onChange={onChangeBody}
+            placeholder={"Write something awesome..."}
+            modules={modules('t1')}
+            formats={formats}
+          />
         </div>
-        <br />
+        <br/>
         {isError !== null && <div> {isError} </div>}
         <div>
           <button className="bg-gray-300 rounded-lg p-1 hover:bg-slate-200" type="submit"> Submit  </button>
